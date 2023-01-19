5f1719bc85d600d640b82909e1172c23ca70d9d9
Miliyonayalew <miliayalew@gmail.com>
2023-01-19 23:13:13 +0300

    Add validation for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,9 @@ class Post < ApplicationRecord
 
   after_save :update_posts_counter
 
+  validates :title, presence: true, length: { in: 1..250 }
+  validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def recent_five_posts
     comments.last(5)
   end
