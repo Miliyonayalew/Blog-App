2272b568c55b6166c4322f835b7b1b6bd495e754
Miliyonayalew <miliayalew@gmail.com>
2023-01-25 02:34:53 +0300

    Implement the design

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
   # GET /users/:user_id/posts
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
   end
 
   # GET /users/:user_id/posts/:id
   def show
-    params[:id]
+    @post = Post.find(params[:id])
   end
 end
