76dc8415825e9238bd9699dde0b2b674ef83d72b
Miliyonayalew <miliayalew@gmail.com>
2023-01-25 21:20:30 +0300

    Add for counter a value of 0 by default

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+first_user = User.create(name: 'Mila', photo: 'https://thumbs.dreamstime.com/b/many-small-links-young-woman-wearing-eye-glasses-208952073.jpg', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'John', photo: 'https://thumbs.dreamstime.com/b/many-small-links-young-woman-wearing-eye-glasses-208952073.jpg', bio: 'Teacher from Poland.')
+first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post')
+comment = Comment.create(post: first_post, author: second_user, text: 'Hi Tom!' )
+like = Like.create(post: first_post, author: second_user)
