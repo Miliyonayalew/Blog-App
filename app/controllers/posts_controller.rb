8b116e23ea4bde7874445ac8772874eafcfa7e47
Miliyonayalew <miliayalew@gmail.com>
2023-01-26 13:35:37 +0300

    Create and update controllers action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,34 @@
 class PostsController < ApplicationController
-  # GET /users/:user_id/posts
   def index
     @user = User.find(params[:user_id])
   end
 
-  # GET /users/:user_id/posts/:id
   def show
+    @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    # create a new post
+    @post = Post.new(post_params)
+    @post.author = current_user
+
+    if @post.save
+      flash[:success] = 'Post created successfully'
+      redirect_to user_posts_path(current_user)
+    else
+      flash[:error] = 'Post not created'
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
