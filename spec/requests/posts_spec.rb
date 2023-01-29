d33cd55aba17a53b091e7f63fa9ee08e418f2ff5
Miliyonayalew <miliayalew@gmail.com>
2023-01-29 15:45:45 +0300

    Fix error in request spec

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,33 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    before(:example) { get users_path(1) }
-    it 'returns http success' do
-      expect(response).to have_http_status(:success)
+  before(:each) do
+    @user = User.create(name: 'mila', photo: 'https://i.imgur.com/1JZ1Q2r.jpg', bio: 'I am a biology teacher',
+                        posts_counter: 2)
+    @post = Post.create(author: @user, title: 'My first post', text: 'This is my first post', comments_counter: 1,
+                        likes_counter: 2)
+  end
+
+  describe 'GET post#index' do
+    before(:each) { get user_posts_path(@user) }
+
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
     end
 
-    it "should render 'index' template" do
-      expect(response).to render_template(:index)
+    it "renders 'index' template" do
+      expect(response).to render_template('index')
     end
 
     it 'should return correct placeholder text' do
-      expect(response.body).to include('User')
+      expect(response.body).to include('Users All Posts')
+    end
+
+    it "does not render a 'show' template" do
+      expect(response).to_not render_template('show')
     end
   end
 
-  describe 'GET /show' do
-    before(:example) { get users_path(1) }
-    it 'returns http success' do
-      expect(response).to have_http_status(:success)
+  describe 'GET post#show' do
+    before(:each) { get user_post_path(@user, @post) }
+
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
     end
 
-    it "should render 'show' template" do
-      expect(response).to render_template(:index)
+    it "renders 'show' template" do
+      expect(response).to render_template('show')
     end
 
     it 'should return correct placeholder text' do
-      expect(response.body).to include('User')
+      expect(response.body).to include('Here is a post description of the user')
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,36 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    before(:example) { get users_path }
-    it 'returns http success' do
-      expect(response).to have_http_status(:success)
+  before(:each) do
+    @user = User.create(name: 'mila', photo: 'https://i.imgur.com/1JZ1Q2r.jpg', bio: 'I am a biology teacher',
+                        posts_counter: 2)
+  end
+
+  describe 'GET #index' do
+    before(:each) { get users_path(@user) }
+
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
     end
 
-    it "should render 'index' template" do
-      expect(response).to render_template(:index)
+    it "renders 'index' template" do
+      expect(response).to render_template('index')
     end
 
     it 'should return correct placeholder text' do
       expect(response.body).to include('Users')
     end
+
+    it "does not render a 'show' template" do
+      expect(response).to_not render_template(:show)
+    end
   end
 
-  describe 'GET /show' do
-    before(:each) do
-      get users_path(id: 1)
+  describe 'GET #show' do
+    before(:each) { get user_path(@user) }
+
+    it 'returns a successful response' do
+      expect(response).to have_http_status(:ok)
     end
 
-    it 'returns http success' do
-      expect(response).to have_http_status(:success)
-    end
-
-    it "should render 'show' template" do
-      expect(response).to render_template(:index)
+    it "renders 'show' template" do
+      expect(response).to render_template('show')
     end
 
     it 'should return correct placeholder text' do
-      expect(response.body).to include('User')
+      expect(response.body).to include('User Post')
     end
   end
 end
