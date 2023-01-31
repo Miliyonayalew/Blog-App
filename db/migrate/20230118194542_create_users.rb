022fe8fb0def64a0e355e2704f50a4bb49d08171
Miliyonayalew <miliayalew@gmail.com>
2023-02-01 00:45:31 +0300

    Generate migration and the views and

--- db/migrate/20230118194542_create_users.rb
+++ db/migrate/20230118194542_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :photo
+      t.string :photo, default: "https://cdn.pixabay.com/photo/2017/01/10/03/54/avatar-1968236_960_720.png"
       t.text :bio
       t.integer :posts_counter, default: 0
 
